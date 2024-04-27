fbdd4cc9ad4d1d5b0106645db4ccf4486907e65b
alex <a>
2024-04-27 12:12:18 +0400

    Bag

---
+++ Source/SnakeGame/Public/SpeedFood.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Food.h"
+#include "SpeedFood.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKEGAME_API ASpeedFood : public AFood
+{
+	GENERATED_BODY()
+	
+};
